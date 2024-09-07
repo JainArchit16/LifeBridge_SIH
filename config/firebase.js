a5cb9c9a289b2d7987bb97e9b91afaea69a554fd
Daksh-134 <117268999+Daksh-134@users.noreply.github.com>
2024-09-08 02:32:36 +0530

    Landing Page part 4

--- config/firebase.js
+++ config/firebase.js
@@ -1,5 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -11,7 +13,10 @@ const firebaseConfig = {
   storageBucket: 'life-bridge.appspot.com',
   messagingSenderId: '252614072994',
   appId: '1:252614072994:web:da95a607e121a63f3a15fb',
+  
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);

86fa172c37a7213cda1ab8ae887ecc98d6b2281f
Archit <jainarchit1604@gmail.com>
2024-09-07 23:42:47 +0530

    Changed

---
+++ config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDZ_ccS9S2dG3PRPUsfZhJruD1q9IUo4q4',
+  authDomain: 'life-bridge.firebaseapp.com',
+  projectId: 'life-bridge',
+  storageBucket: 'life-bridge.appspot.com',
+  messagingSenderId: '252614072994',
+  appId: '1:252614072994:web:da95a607e121a63f3a15fb',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);

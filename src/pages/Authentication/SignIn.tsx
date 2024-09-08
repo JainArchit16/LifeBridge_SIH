ee4544341e4bdbd5384c10d5ce2224c112db241a
Archit <jainarchit1604@gmail.com>
2024-09-08 15:12:10 +0530

    .

--- src/pages/Authentication/SignIn.tsx
+++ src/pages/Authentication/SignIn.tsx
@@ -366,7 +366,7 @@ const SignIn: React.FC = () => {
                       name="password"
                       value={formData.password}
                       onChange={handleOnChange}
-                      placeholder="6+ Characters, 1 Capital letter"
+                      placeholder="6+ Characters"
                       className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
                       required
                     />

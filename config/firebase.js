fcc53919d1fa3d6b7fb0f2aa3ab58bff599043fe
Ayush Arora <ayusharora.282@gmail.com>
2024-09-08 12:58:07 +0530

    reviews done

--- config/firebase.js
+++ config/firebase.js
@@ -2,11 +2,8 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-<<<<<<< HEAD
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-=======
->>>>>>> 395a5c3dbd52491d3f8b39711e2865c0c1317325
 
 const firebaseConfig = {
   apiKey: 'AIzaSyDZ_ccS9S2dG3PRPUsfZhJruD1q9IUo4q4',
@@ -15,15 +12,10 @@ const firebaseConfig = {
   storageBucket: 'life-bridge.appspot.com',
   messagingSenderId: '252614072994',
   appId: '1:252614072994:web:da95a607e121a63f3a15fb',
-  
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-<<<<<<< HEAD
-export const db = getFirestore(app);
-=======
 export default app;
 export const db = getFirestore(app);
->>>>>>> 395a5c3dbd52491d3f8b39711e2865c0c1317325
--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,13 @@ import App from './App';
 
 import 'jsvectormap/dist/css/jsvectormap.css';
 import 'flatpickr/dist/flatpickr.min.css';
+import { Toaster } from 'react-hot-toast';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Router>
       <App />
+      <Toaster />
     </Router>
   </React.StrictMode>,
 );
